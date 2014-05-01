a893f520dd8629a52c0eed20ba431ebb1f8d3df7
Lin He <he9lin@gmail.com>
2014-04-30 17:22:23 -0700

    Add logs

--- files/default/plugins/vboxipaddress.rb
+++ files/default/plugins/vboxipaddress.rb
@@ -3,12 +3,14 @@ Ohai.plugin(:Vboxipaddress) do
   depends "ipaddress", "network/interfaces", "virtualization/system", "etc/passwd"
 
   collect_data(:default) do
+    Ohai::Log.info("Vboxipaddress working hard...")
     if virtualization["system"] == "vbox"
       if etc["passwd"].any? { |k,v| k == "vagrant"}
         if network["interfaces"]["eth1"]
           network["interfaces"]["eth1"]["addresses"].each do |ip, params|
             if params['family'] == ('inet')
               ipaddress ip
+              Ohai::Log.info("ipaddress for vbox is #{ip}")
             end
           end
         end
